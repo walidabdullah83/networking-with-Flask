b1c908de570c19d47ad0020a1187626c75227418
whafez <wabdelhafeez.c@stcs.com.sa>
2022-09-11 05:05:23 +0300

    adding Jinja2 templates to HTML

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
-#Testing Branching
+import json
 
 app = Flask(__name__)
+
+
+def load_db():
+    with open("sample_db.json") as f:
+        return json.load(f)
+
+
+db = load_db()
+
 
 #the view section to show the HTML page
 @app.route("/")
 def welcome():
-    return render_template("main.html")
-
-if __name__ == '__main__':
-   app.run()
+    entries = db
+    return render_template("main.html", entries=entries)
